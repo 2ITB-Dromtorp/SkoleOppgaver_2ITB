9827187bf2a9b9015221146d30e86ce434f351d6
Joakim Husefest <joakimhus@viken.no>
2023-08-28 14:22:38 +0200

    last version of react intro

--- react_intro/react_intro/src/myButton.js
+++ react_intro/react_intro/src/myButton.js
@@ -1,30 +1,40 @@
 import './App.css';
 
-function useList() {
-
-  alert('This is this buttons function');
-
+function clickButton() {
+  console.log('Button clicked');
 }
 
-export default function MyButton() {
+function Uselist() {
 
-  let products = [
-    { title: 'Cabbage', id: 1 },
-    { title: 'Garlic', id: 2 },
-    { title: 'Apple', id: 3 },
+  const products = [
+    { title: 'Cabbage', isFruit: false, id: 1 },
+    { title: 'Garlic', isFruit: false, id: 2 },
+    { title: 'Apple', isFruit: true, id: 3 },
   ];
 
   const listItems = products.map(product =>
-    <li key={product.id}>
-      {product.title}
-    </li>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+    }}
+  >
+    {product.title}
+  </li>
   );
 
+  return <ul>{listItems}</ul>
+  
+}  
+
+export default function MyButton() {
+
+  
     return (
       <div>
         <h1>Welcome to my app</h1>
-        <button className='button' onClick={useList}>I'm a button</button>
-        <ul>{listItems}</ul>
+        <button className='button' onClick={clickButton}>I'm a button</button>
+        <Uselist />
       </div>
     );
   }
