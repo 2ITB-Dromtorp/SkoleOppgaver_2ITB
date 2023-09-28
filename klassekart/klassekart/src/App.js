ae058668a281f52164d0bd271d4e74a592699fef
Joakim Husefest <joakimhus@viken.no>
2023-09-28 08:41:07 +0200

    started klassekart

---
+++ klassekart/klassekart/src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      <div className="container">
+          
+          <div className='tositteplasser'>
+            <div className='elev'> Elev </div>
+            <div className='elev'> Elev </div>
+          </div>
+
+          <div className='tositteplasser'>
+            <div className='elev'> Elev </div>
+            <div className='elev'> Elev </div>
+          </div>
+
+          <div className='tositteplasser'>
+            <div className='elev'> Elev </div>
+            <div className='elev'> Elev </div>
+          </div>
+
+      </div>
+
+        <div className="container">
+          kaokaokak  a+ooasoaa
+        </div>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
