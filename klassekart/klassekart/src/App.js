0f7d9b9a7a4f00f8c6db68d83d89184104a27047
Joakim Husefest <joakimhus@viken.no>
2023-09-28 09:11:48 +0200

    css looks better

--- klassekart/klassekart/src/App.js
+++ klassekart/klassekart/src/App.js
@@ -4,38 +4,62 @@ import './App.css';
 function App() {
 
   return (
-    <div className="App">
-      <header className="App-header">
+
 
         <div className="container">
 
-          <div>
+        <div className='leftside'>
+
+            <div className='box'>
+
+              <div className='tositteplasser'> 
+                <div className='elev'> Elev </div>
+                <div className='elev'> Elev </div>
+              </div>
+
+              <div className='tositteplasser'> 
+                <div className='elev'> Elev </div>
+                <div className='elev'> Elev </div>
+              </div>
+
+              <div className='tositteplasser'> 
+                <div className='elev'> Elev </div>
+                <div className='elev'> Elev </div>
+              </div>
+
+            </div>
+          </div>
             
-            <div className='tositteplasser'>
-              <div className='elev'> Elev </div>
-              <div className='elev'> Elev </div>
-            </div>
 
-            <div className='tositteplasser'>
-              <div className='elev'> Elev </div>
-              <div className='elev'> Elev </div>
-            </div>
+        <div className='rightside'>
 
-            <div className='tositteplasser'>
-              <div className='elev'> Elev </div>
-              <div className='elev'> Elev </div>
-            </div>
+        <div className='box'>
+
+            <div className='tresitteplasser'> 
+                <div className='elev'> Elev </div>
+                <div className='elev'> Elev </div>
+                <div className='elev'> Elev </div>
+              </div>
+
+              <div className='tresitteplasser'> 
+                <div className='elev'> Elev </div>
+                <div className='elev'> Elev </div>
+                <div className='elev'> Elev </div>
+              </div>
+
+              <div className='tresitteplasser'> 
+                <div className='elev'> Elev </div>
+                <div className='elev'> Elev </div>
+                <div className='elev'> Elev </div>
+              </div>
+            
+          </div>
+
+          </div>
+
 
         </div>
 
-        </div>
-
-        <div className="container">
-          kaokaokak  a+ooasoaa
-        </div>
-
-      </header>
-    </div>
   );
 }
 
