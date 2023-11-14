421345f14d523343cf9b1b8e30d4d26d9a5676e9
Joakim Tveit Husefest <joakimhus@viken.no>
2023-11-14 13:39:02 +0100

    node applications

--- nodeApplications/helloWorld/index.js
+++ nodeApplications/helloWorld/index.js
@@ -1,6 +1,30 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'johu',
+  password: 'dromtorp',
+  database: 'dromtorp'
+});
+
+connection.connect(function(err) {
+
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
+
+connection.query('SELECT * FROM elev', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results);
+});
 
 app.get('/', (request, response) => {
   response.send('Hello World!!!!!!')
@@ -9,3 +33,4 @@ app.get('/', (request, response) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
